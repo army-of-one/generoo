187943ec9782a93ec9619fbe7b8bba1e1aeed1f2
Thomas Sickert <thomas.sickert@gmail.com>
2019-05-01 00:05:21 -0500

    Removed Old Implementations

--- archetypes/__init__.py
+++
@@ -1,8 +0,0 @@
-from os.path import dirname, basename, isfile, join
-import glob
-
-before_file_extension = -len(".py")
-
-modules = glob.glob(join(dirname(__file__), "*.py"))
-
-__all__ = [basename(f)[:before_file_extension] for f in modules if isfile(f) and not f.endswith('__init__.py')]
