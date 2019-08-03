67f99fe6c5e27672de90e0711c3393a00699e282
Thomas Sickert <thomas.sickert@gmail.com>
2019-08-03 07:19:39 -0600

    Added Missing Conditional Flag to Archetype

--- generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/main/java/{{group_id_slashes}}/{{artifact_id_slashes}}/{{artifact_id_capitalized}}Application.java
+++ generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/main/java/{{group_id_slashes}}/{{artifact_id_slashes}}/{{artifact_id_capitalized}}Application.java
@@ -2,14 +2,18 @@ package {{group_id}}.{{artifact_id_periods}};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+{{#hibernate}}
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+{{/hibernate}}
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(scanBasePackages = {
 	"{{group_id}}.{{artifact_id_periods}}"
 })
 @EnableSwagger2
+{{#hibernate}}
 @EnableJpaRepositories(basePackages = "{{group_id}}.{{artifact_id_periods}}.repository")
+{{/hibernate}}
 public class {{artifact_id_capitalized}}Application {
 
 	public static void main(String[] args) {
--- generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/test/java/{{group_id_slashes}}/{{artifact_id_slashes}}/service/{{artifact_id_capitalized}}ServiceImpTest.java
+++ generoo/archetypes/java/spring-boot/2.0/{{artifact_id}}-api/src/test/java/{{group_id_slashes}}/{{artifact_id_slashes}}/service/{{artifact_id_capitalized}}ServiceImpTest.java
@@ -1,6 +1,8 @@
 package {{group_id}}.{{artifact_id_periods}}.service;
 
+{{#hibernate}}
 import {{group_id}}.{{artifact_id_periods}}.repository.{{artifact_id_capitalized}}Repository;
+{{/hibernate}}
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,9 +12,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class {{artifact_id_capitalized}}ServiceImpTest {
 
+  {{#hibernate}}
   @Mock
   private {{artifact_id_capitalized}}Repository {{artifact_id_camel}}Repository;
-
+  {{/hibernate}}
   @InjectMocks
   private {{artifact_id_capitalized}}Service {{artifact_id_camel}}ServiceImp;
 
